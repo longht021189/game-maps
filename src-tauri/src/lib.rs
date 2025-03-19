05259c7c4fbd6b2b25a4a16a78c976738c8e5153
Thanh Long <47407870+longht021189@users.noreply.github.com>
2025-03-19 22:33:49 +0700

    update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,7 +7,24 @@ extern "C" {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let host = if cfg!(dev) {
+        "http://localhost:3000"
+    } else {
+        "https://mapgenie.io"
+    };
+
+    println!(">>>>>>>>>> host={}", host);
+
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(dev)]
+            {
+                // let window = app.get_webview_window("main").unwrap();
+                // window.open_devtools();
+            }
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .build(tauri::generate_context!())
         .expect("error while build tauri application")
